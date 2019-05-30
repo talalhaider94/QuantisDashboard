13c3f44b11a773f73cb86012bbec19b8508956ee
Talal Haider <talalhaider93@gmail.com>
2019-05-30 15:41:24 +0500

    Dashboard Files Update

--- Quantis.Workflow.Complete/ClientApp/src/app/_nav.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/_nav.ts
@@ -39,68 +39,23 @@ export const navItems: NavData[] = [
       text: 'NEW'
     }
   },
-  // {
-  //   name: 'Base',
-  //   url: '/base',
-  //   icon: 'icon-puzzle',
-  //   children: [
-  //     {
-  //       name: 'Cards',
-  //       url: '/base/cards',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Carousels',
-  //       url: '/base/carousels',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Collapses',
-  //       url: '/base/collapses',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Forms',
-  //       url: '/base/forms',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Pagination',
-  //       url: '/base/paginations',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Popovers',
-  //       url: '/base/popovers',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Progress',
-  //       url: '/base/progress',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Switches',
-  //       url: '/base/switches',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Tables',
-  //       url: '/base/tables',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Tabs',
-  //       url: '/base/tabs',
-  //       icon: 'icon-puzzle'
-  //     },
-  //     {
-  //       name: 'Tooltips',
-  //       url: '/base/tooltips',
-  //       icon: 'icon-puzzle'
-  //     }
-  //   ]
-  // },
+  {
+    name: 'Home',
+    url: '/base',
+    icon: 'icon-note',
+    children: [
+      {
+        name: 'Contraenti',
+        url: '',
+        icon: 'icon-circle'
+      },
+      {
+        name: 'Contratti',
+        url: '',
+        icon: 'icon-circle'
+      }
+    ]
+  },
   {
     divider: true
   },
